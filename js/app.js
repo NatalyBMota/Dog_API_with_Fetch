1c3f69ec146c84cea56f7d245acb66e5d05a94ae
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2023-09-07 19:28:31 -0500

    Removed excessively long comments from the app.js file, and moved them to the README.md documentation file instead.

--- js/app.js
+++ js/app.js
@@ -98,15 +98,7 @@ function postData(e) {
     },
     body: JSON.stringify({ name, comment })
   };
-  
-  /* 
-    - method indicates the type of request. 
-    - The headers for the request are usually contained within an object. 
-    - application/json is the media type for a JSON response. This communicates to the server that the data has been encoded with JSON.
-    - In POST requests, values are sent to the server in the body of the request. First, the form data needs to be "strigified," or transformed into a JSON string.
-    - The method JSON.stringify() here is being used to convert the values of name and comment into a JSON string.
-        -- These above comments are quotes by Guil Hernandez, from his lesson on "Posting Data with fetch()" in his "Working with the Fetch API" module of the "Intro to Programming" course at Team Tree House. They came from notes that I jotted down while following along with his interactive and video-based tutorial, which I used to produce this project.
-  */
+    
   fetch('https://jsonplaceholder.typicode.com/comments', config)
     .then(checkStatus)
     .then(res => res.json())
